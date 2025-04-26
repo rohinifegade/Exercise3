eb2ac045e532a724d9bb3e1ad5971cd1bf84bb69
rohinifegade <rohini.fegade01@gmail.com>
2025-04-26 03:37:29 -0700

    created task 2

---
+++ Task2.py
@@ -0,0 +1,11 @@
+#Using the Math Module for Calculations.
+
+import math
+num=int(input("Enter a Number :"))
+s=math.sqrt(num)
+print("Square Root:",s)
+log=math.log(num)
+print("Logarithm:",log)
+sine=math.sin(num)
+print("sine:",sine)
+
